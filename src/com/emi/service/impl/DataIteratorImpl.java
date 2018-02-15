c17a4e938be3309efdd9cf7bb9311022f2e63d53
amareshbonam <36504344+amareshbonam@users.noreply.github.com>
2018-02-15 23:30:15 +0530

    Update DataIteratorImpl.java

--- src/com/emi/service/impl/DataIteratorImpl.java
+++ src/com/emi/service/impl/DataIteratorImpl.java
@@ -12,59 +12,6 @@ public class DataIteratorImpl implements DataIterator{
 	public static DataIterator getInstance() {
 		return new DataIteratorImpl();
 	}
-		
-	/*public void iterateOverJSONArray(StringBuilder value, JsonArray jsonArray, int index){
-		for(int i=0; i<3; i++) {
-			if(jsonArray.size() > index ) {
-				value.append(jsonArray.get(index));
-				index++;
-				iterateOverJSONArray(value, jsonArray, index);
-			}
-		}
-	}
-	
-	public void iterateOverJSONObject(List<String> values, StringBuilder builder, JsonObject jsonObject, int keyIndex, String[] keys){
-		for(;keyIndex < keys.length; keyIndex++) {
-			JsonArray jsonArray = jsonObject.getJsonArray(keys[keyIndex]);
-			for(int i=0; i<jsonArray.size(); i++) {
-//				if(!StringBuilderUtil.isValidObject(builder))
-//					builder = new StringBuilder();
-				if(keys.length > keyIndex+1) {
-					builder.append(jsonArray.get(i));
-					iterateOverJSONObject(values, builder, jsonObject, keyIndex+1, keys);
-					builder = new StringBuilder();
-				} else if(DataUtil.isValidObject(builder)){ 
-//					if(builder.length()+1 ==  keys.length)
-						values.add(builder.toString().concat(jsonArray.get(i).toString()));
-//					if(keyIndex+1 == 3 && i+1 == jsonArray.size())
-//						break;
-				}
-			}
-		}
-	}
-	
-	public void iterateOverJSONObject(JsonObject jsonObject, String[] keys) {
-		List<String> oldList = new ArrayList<String>();
-		for(int i=keys.length-1; i>=0; i--) {
-			List<String> newList = new ArrayList<String>();
-			JsonArray jsonArray = jsonObject.getJsonArray(keys[i]);
-			addArrayToList(jsonArray, oldList, newList);
-			oldList = newList;
-		}
-	}
-	
-	public List<String> addArrayToList(JsonArray jsonArray,List<String> oldList, List<String> newList) {
-		for(int j=0; j<jsonArray.size(); j++) {
-			if(DataUtil.isValidList(oldList)) {
-				for(int i=0; i<oldList.size(); i++) {
-					newList.add(jsonArray.get(j).toString().concat(oldList.get(i)));
-				}
-			} else {
-				newList.add(jsonArray.get(j).toString());
-			}
-		}
-		return newList;
-	}*/
 	
 	@Override
 	public List<String> addArrayToList(List<String> list, List<String> oldList, List<String> newList) {
@@ -82,7 +29,6 @@ public class DataIteratorImpl implements DataIterator{
 
 	@Override
 	public void iterateOverDataObject(List<String> oldList, Map<Integer, List<String>> dataMap, int[] keys, int index) {
-//		for(int i=keys.length-1; i>=0; i--) {
 		List<String> newList = new ArrayList<String>();
 		List<String> list = dataMap.get(keys[index]);
 		addArrayToList(list, oldList, newList);
